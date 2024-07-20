1e4f83b74408a085ff1dfac099ad3dd5d2a70654
Dũng T. Phạm <dung.pham@del-qui.com>
2024-07-20 18:40:58 +0700

    chore(log): Add catch all logging in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logging/logging.dart';
 
 import 'src/app.dart';
 
 void main() async {
   await dotenv.load();
+  var log = Logger("Main");
+
+  FlutterError.onError = (details) {
+    FlutterError.presentError(details);
+    log.severe(
+      'FlutterError - Catch all',
+      "${details.toString()}\nException: ${details.exception}\nLibrary: ${details.library}\nContext: ${details.context}",
+      details.stack,
+    );
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    log.severe('PlatformDispatcher - Catch all', error, stack);
+    return true;
+  };
+
   runApp(const ProviderScope(
     child: KyoryoApp(),
   ));
