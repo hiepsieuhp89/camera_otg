f40ab348b17f01d1eda4e7c5fa76924970d6b2c0
Dũng T. Phạm <dung.pham@del-qui.com>
2024-11-11 04:38:45 +0700

    KYORYO-461 feat(inspection): Add a modal route for image marking

--- lib/src/models/marking.dart
+++ lib/src/models/marking.dart
@@ -3,4 +3,16 @@ class Marking {
   final int y;
 
   const Marking({required this.x, required this.y});
+
+  Marking copyWith({int? x, int? y}) {
+    return Marking(
+      x: x ?? this.x,
+      y: y ?? this.y,
+    );
+  }
+
+  @override
+  toString() {
+    return 'Marking(x: $x, y: $y)';
+  }
 }
