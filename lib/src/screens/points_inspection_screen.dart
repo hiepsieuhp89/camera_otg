f9f39afdddfe2754b33b32e178c8adb638750c5c
Dũng T. Phạm <dung.pham@del-qui.com>
2025-01-17 17:52:49 +0700

    fix: Incorrect damage type value when NON category is selected
    fix: points in points inspection screen is not updated after edit
    
    fix: Add details in points inspection screen app bar

--- lib/src/screens/points_inspection_screen.dart
+++ lib/src/screens/points_inspection_screen.dart
@@ -1,15 +1,18 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kyoryo/src/models/inspection_point.dart';
+import 'package:kyoryo/src/localization/app_localizations.dart';
 import 'package:kyoryo/src/providers/current_bridge.provider.dart';
+import 'package:kyoryo/src/providers/inspection_points.provider.dart';
 import 'package:kyoryo/src/ui/inspection_point_list_item.dart';
 
 @RoutePage()
 class PointsInspectionScreen extends ConsumerStatefulWidget {
-  final List<InspectionPoint> points;
+  final List<int> pointIds;
+  final String? details;
 
-  const PointsInspectionScreen({super.key, required this.points});
+  const PointsInspectionScreen(
+      {super.key, required this.pointIds, this.details});
 
   @override
   PointsInspectionScreenState createState() => PointsInspectionScreenState();
@@ -22,26 +25,70 @@ class PointsInspectionScreenState
     final currentBridge = ref.watch(currentBridgeProvider);
 
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           title: Row(
-        children: <Widget>[
-          Text(currentBridge?.nameKanji ?? ''),
-          IconButton(
-              onPressed: () {}, icon: const Icon(Icons.info_outline_rounded)),
-        ],
-      )),
-      body: ListView.builder(
-        key: const PageStorageKey<String>('inspection-point-list'),
-        itemCount: widget.points.length,
-        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-        itemBuilder: (BuildContext context, int index) {
-          final point = widget.points[index];
+            spacing: 8,
+            children: <Widget>[
+              Text(currentBridge?.nameKanji ?? ''),
+              if (widget.details != null)
+                Text(widget.details!,
+                    style: Theme.of(context).textTheme.titleSmall),
+            ],
+          ),
+        ),
+        body: ref.watch(inspectionPointsProvider(currentBridge!.id)).when(
+              data: (allPoints) {
+                final points = allPoints
+                    .where((point) => widget.pointIds.contains(point.id))
+                    .toList();
 
-          return InpsectionPointListItem(
-            point: point,
-          );
-        },
-      ),
-    );
+                return RefreshIndicator(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: ListView.builder(
+                      key:
+                          const PageStorageKey<String>('inspection-point-list'),
+                      itemCount: points.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final point = points[index];
+
+                        return InpsectionPointListItem(
+                          point: point,
+                        );
+                      },
+                    ),
+                  ),
+                  onRefresh: () => Future.wait([
+                    ref.refresh(
+                        inspectionPointsProvider(currentBridge.id).future),
+                  ]),
+                );
+              },
+              loading: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
+              error: (error, stackTrace) {
+                debugPrint('Error: $error, $stackTrace');
+
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                          AppLocalizations.of(context)!
+                              .failedToGetInspectionPoints,
+                          style: Theme.of(context).textTheme.bodySmall),
+                      IconButton(
+                        onPressed: () {
+                          ref.invalidate(
+                              inspectionPointsProvider(currentBridge.id));
+                        },
+                        icon: const Icon(Icons.refresh),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ));
   }
 }
