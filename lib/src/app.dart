5b1e1865c72b77443c8d2cc932e631abc10d9227
Maverick <tungcx_aivn@binhminh.com.vn>
2025-05-19 17:55:03 +0700

    init2

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lavie/src/routes/app_router.dart';
 import 'package:lavie/src/theme/app_theme.dart';
+import 'package:auto_route/auto_route.dart';
 
 class LavieApp extends ConsumerWidget {
   const LavieApp({super.key});
@@ -16,7 +17,8 @@ class LavieApp extends ConsumerWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      routerConfig: appRouter,
+      routerDelegate: appRouter.delegate(),
+      routeInformationParser: appRouter.defaultRouteParser(),
     );
   }
 }
