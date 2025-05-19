2d0720914719d97be36ecc857f5805a8e40b4559
Maverick <tungcx_aivn@binhminh.com.vn>
2025-05-19 18:04:12 +0700

    init2

--- lib/src/routes/app_router.dart
+++ lib/src/routes/app_router.dart
@@ -9,7 +9,7 @@ import 'package:lavie/src/features/viewer/presentation/viewer_screen.dart';
 part 'app_router.gr.dart';
 
 @AutoRouterConfig()
-class AppRouter extends _$AppRouter {
+class AppRouter extends RootStackRouter {
   List<AutoRoute> get routes => [
         AutoRoute(page: SplashRoute.page, initial: true),
         AutoRoute(page: LoginRoute.page),
